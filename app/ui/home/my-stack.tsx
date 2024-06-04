ac9f51116fcabaf48088b4730e1b62ccb564b388
Miguel Franco <miguel.franco@pepsico.com>
2024-06-03 22:28:06 -0600

    feat: added scroll bar css and fixed my stack space

--- app/ui/home/my-stack.tsx
+++ app/ui/home/my-stack.tsx
@@ -5,11 +5,14 @@ import Image from 'next/image';
 const MyStack = () => {
   return (
     <div className='flex flex-col text-center pt-10'>
-      <p className='text-4xl py-8'>My Stack</p>
-      <div className='flex mobile:flex-col laptop:flex-row justify-center gap-10'>
+      <p className='text-4xl py-8'>My Stack 👨🏽‍💻</p>
+      <div className='flex mobile:flex-col laptop:flex-row flex-wrap justify-between gap-10'>
         {Object.keys(myStack).map((header, index) => (
-          <div key={`${header}-${index}`}>
-            <p className='border-x-2 px-4'>{header.toUpperCase()}</p>
+          <div
+            key={`${header}-${index}`}
+            className='laptop:w-3/12'
+          >
+            <p className='border-x-2 px-8'>{header.toUpperCase()}</p>
             <ul className='justify-start text-center mt-5 mobile:max-laptop:max-h-44 laptop:max-h-44 overflow-y-auto'>
               {Object.entries(myStack[header].technologies).map(
                 ([technologyName, technologyIcon], itemIndex: number) => {
